1a563da3b7e929b5cb459de2ed710669fcd7ea3d
SlavaPachevsky <savaukr17101980@gmail.com>
2022-07-13 18:34:52 +0300

    city card

--- src/components/cityCard/cityCard.tsx
+++ src/components/cityCard/cityCard.tsx
@@ -1,35 +1,79 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { Button } from "@material-ui/core";
 
-import "./cityCard.css";
 import { ICity, IFullCity } from "../../types/city_types";
 
+import { makeStyles } from "@material-ui/core/styles";
+import { Button } from "@material-ui/core";
+import DeleteIcon from "@material-ui/icons/Delete";
+
+import "./cityCard.css";
+
 const api_key = "4b456c4b39aae41889a3f1fa952bb6ff";
+
+const useStyles = makeStyles((theme) => ({
+  button: {
+    margin: theme.spacing(1),
+  },
+}));
+
 type Props = {
   city: ICity;
+  className: string;
 };
-const CityCard = ({ city }: Props) => {
+
+const CityCard = ({ city, className }: Props) => {
+  const classes = useStyles();
+  const [isLoading, setIsloading] = useState(false);
   const [stateCity, setCity] = useState<IFullCity | null>(null);
   const url = `https://api.openweathermap.org/data/2.5/weather?id=${city.id}&appid=${api_key}`;
   async function fetchFullCity() {
     try {
+      setIsloading(true);
       const response = await axios.get<IFullCity>(url);
       setCity(response.data);
-    } catch (err) {}
+    } catch (err) {
+    } finally {
+      setIsloading(false);
+    }
   }
   useEffect(() => {
     fetchFullCity();
   }, []);
 
-  const updateCity = () => {
-    fetchFullCity();
+  const updateCity = async () => {
+    await fetchFullCity();
   };
+
+  const deleteCity = async () => {
+    console.log("delete");
+  };
+
   return (
-    <div className="cityCardWrapper">
-      <h1>{stateCity?.name}</h1>
-      <Button variant="outlined" onClick={updateCity} color={"primary"}>
+    <div className={className}>
+      <h3>
+        {stateCity?.name ? stateCity.name : null}
+        {"Kyiv"}
+      </h3>
+      <div>
+        {stateCity?.main?.temp ? `${stateCity.main.temp - 273}  \u00b0` : null}
+        {`273 \u00b0`}
+      </div>
+      <Button
+        variant="contained"
+        onClick={updateCity}
+        color={"primary"}
+        className={classes.button}
+      >
         Update
+      </Button>
+      <Button
+        variant="outlined"
+        onClick={deleteCity}
+        color="secondary"
+        startIcon={<DeleteIcon />}
+      >
+        Delete
       </Button>
     </div>
   );
--- src/components/cityList/cityList.tsx
+++ src/components/cityList/cityList.tsx
@@ -34,41 +34,48 @@ const CityList = () => {
 
   return (
     <div className="tableWrapper">
-      <Container maxWidth="sm">
+      <div className="container">
         <h1>Cities list</h1>
         {loading ? <CircularProgress /> : ""}
         {error ? <h1>Sorry, an error occurred</h1> : null}
-
-        {!error && !loading
-          ? cities.map((city) => <CityCard city={city} key={city.id} />)
-          : // <TableContainer component={Paper}>
-            //   <Table
-            //     className={classes.table}
-            //     size="small"
-            //     aria-label="a dense table"
-            //   >
-            //     <TableHead>
-            //       <TableRow>
-            //         <TableCell align="center">id</TableCell>
-            //         <TableCell align="center">name</TableCell>
-            //         <TableCell align="center">state</TableCell>
-            //         <TableCell align="center">country</TableCell>
-            //       </TableRow>
-            //     </TableHead>
-            //     <TableBody>
-            //       {cities.map((city) => (
-            //         <TableRow key={city.id} className={"pointer"}>
-            //           <TableCell align="center">{city.id}</TableCell>
-            //           <TableCell align="center">{city.name}</TableCell>
-            //           <TableCell align="center">{city.state}</TableCell>
-            //           <TableCell align="center">{city.country}</TableCell>
-            //         </TableRow>
-            //       ))}
-            //     </TableBody>
-            //   </Table>
-            // </TableContainer>
-            null}
-      </Container>
+        <div className="cards">
+          {!error && !loading
+            ? cities.map((city) => (
+                <CityCard
+                  city={city}
+                  key={city.id}
+                  className="cityCardWrapper"
+                />
+              ))
+            : // <TableContainer component={Paper}>
+              //   <Table
+              //     className={classes.table}
+              //     size="small"
+              //     aria-label="a dense table"
+              //   >
+              //     <TableHead>
+              //       <TableRow>
+              //         <TableCell align="center">id</TableCell>
+              //         <TableCell align="center">name</TableCell>
+              //         <TableCell align="center">state</TableCell>
+              //         <TableCell align="center">country</TableCell>
+              //       </TableRow>
+              //     </TableHead>
+              //     <TableBody>
+              //       {cities.map((city) => (
+              //         <TableRow key={city.id} className={"pointer"}>
+              //           <TableCell align="center">{city.id}</TableCell>
+              //           <TableCell align="center">{city.name}</TableCell>
+              //           <TableCell align="center">{city.state}</TableCell>
+              //           <TableCell align="center">{city.country}</TableCell>
+              //         </TableRow>
+              //       ))}
+              //     </TableBody>
+              //   </Table>
+              // </TableContainer>
+              null}
+        </div>
+      </div>
     </div>
   );
 };
